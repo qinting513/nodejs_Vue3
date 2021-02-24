449ba68a3fceff8fc92e2d6aacdc48be147eeaad
qinting <513924632@qq.com>
2021-02-24 22:54:31 +0800

    修改代码

---
+++ Nodejs/3.CommonJS/module/request.js
@@ -0,0 +1,25 @@
+
+// exports.get=function(){
+
+//     console.log('从服务器获取数据')
+// }
+
+// exports.post=function(){
+
+//     console.log('提交数据')
+// }
+
+
+
+var obj={
+
+    get:function(){
+
+        console.log('从服务器获取数据')
+    },
+    post:function(){
+        console.log('提交数据')
+    }
+}
+
+module.exports=obj;
